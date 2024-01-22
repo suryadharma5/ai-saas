84fd75b7759f6a7b98ee6bfca9df07e94a7f9b1d
suryadharma5 <suryadharmasetiawan03@gmail.com>
2024-01-22 15:57:33 +0700

    feat: sidebar item and mobile sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 import React from 'react'
 
 const DashboardLayout = ({
@@ -9,10 +11,11 @@ const DashboardLayout = ({
         <div className='h-full relative'>
             <div className='hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900'>
                 <div>
-                    Hello Sidebar
+                    <Sidebar />
                 </div>
             </div>
             <main className='md:pl-72'>
+                <Navbar />
                 {children}
             </main>
         </div>
